2d8ed7d003012294963c9f10f32f57d8f75f4a18
suryanshp1 <suryanshp1@gmail.com>
2026-01-23 22:44:15 +0530

    feat: merged changes from feature/a

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ function add(a, b) {
     return a+b
 }
 
+function sub(a, b) {
+    if (!a>b) throw new Error('number a must be greated than b');
+    
+    return a-b
+}
+
 x = add(2,3)
 print(x)
 add(10,33)
 add(13,5)
+
+
+sub(10, 3)
