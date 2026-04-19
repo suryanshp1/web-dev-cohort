d9a87f26670456dc2b05db3e79fbcd32b3026b66
suryanshp1 <suryanshp1@gmail.com>
2026-04-19 16:46:58 +0530

    feat: initial project setup

--- prototypes/backend-cohort/src/common/config/email.js
+++ prototypes/backend-cohort/src/common/config/email.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to, subject, html) => {
     await transporter.sendMail({
-        from: `"${process.env.SMTP_FROM_EMAIL}" <${process.env.SMTP_FROM_EMAIL}>`,
+        from: `"${process.env.SMTP_FROM_NAME}" <${process.env.SMTP_FROM_EMAIL}>`,
         to,
         subject,
         html,
@@ -28,7 +28,31 @@ const sendVerificationEmail = async (email, token) => {
     )
 }
 
+const sendResetPasswordEmail = async (email, token) => {
+    const url = `${process.env.CLIENT_URL}/verify-email/${token}`;
+    await sendMail(
+        email,
+        "Reset your password",
+        `<h2>Password Reset</h2><p>Click <a href="${url}">here</a> to reset your password. This link will be expired in 15 minutes.</p>`,
+    )
+}
+
+const sendOrderConfirmationEmail = async (email, order) => {
+    const items = order.items
+        .map((i) => `<li>${i.title} x${i.quantity} -  Rs${i.price}</li>`)
+        .join("");
+
+    await sendMail(
+        email,
+        `Order Confirmed - ${order.orderNumber}`,
+        `<h2>Order Confirmed!</h2>
+        <p>Order: ${order.orderNumber}</p>`,
+    )
+}
+
 export {
     sendEmail,
     sendVerificationEmail,
+    sendResetPasswordEmail,
+    sendOrderConfirmationEmail,
 }
--- prototypes/backend-cohort/src/modules/auth/auth.routes.js
+++ prototypes/backend-cohort/src/modules/auth/auth.routes.js
@@ -4,12 +4,18 @@ import validate from "../../common/middleware/validate.middleware.js"
 import RegisterDto from "./dto/register.dto.js"
 import { authenticate } from "./auth.middleware.js"
 import LoginDto from "./dto/login.dto.js"
+import ResetPasswordDto from "./dto/reset-password.dto.js"
+import ForgotPasswordDto from "./dto/forgot-password.dto.js"
 
 const router = Router()
 
 router.post("/register", validate(RegisterDto) ,controller.register)
 router.post("/login", validate(LoginDto), controller.login)
 router.post("/logout", authenticate, controller.logout)
+router.get("/verify-email/:token", controller.verifyEmail)
+router.get("/forgot-password", validate(ForgotPasswordDto), controller.forgotPassword)
+router.get("/reset-passord/:token", validate(ResetPasswordDto), controller.resetPassword)
 router.get("/me", authenticate, controller.getMe)
+
 
 export default router;
